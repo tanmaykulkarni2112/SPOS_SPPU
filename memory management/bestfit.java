4ac2c012d3a5abab70d1e3c777fd95d052b71563
Tanmay Kulkarni <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-11-07 23:08:04 +0530

    Update bestfit.java

--- memory management/bestfit.java
+++ memory management/bestfit.java
@@ -1,62 +1,72 @@
 import java.util.Scanner;
 
-public class bestfit {
-    static void bestFit(int blockSize[], int m, int processSize[], int n) {
-        int allocation[] = new int[n]; // Array to store block allocation
-        for (int i = 0; i < allocation.length; i++) allocation[i] = -1;
-
-        for (int i = 0; i < n; i++) {
-            int bestIdx = -1;
-            for (int j = 0; j < m; j++) {
-                if (blockSize[j] >= processSize[i]) {
-                    if (bestIdx == -1 || blockSize[bestIdx] > blockSize[j])
-                        bestIdx = j;
-                }
-            }
-
-            if (bestIdx != -1) {
-                allocation[i] = bestIdx;
-                blockSize[bestIdx] -= processSize[i];
-            }
-        }
-
-        System.out.println("\nProcess No.\tProcess Size\tBlock no.");
-        for (int i = 0; i < n; i++) {
-            System.out.print(" " + (i + 1) + "\t\t" + processSize[i] + "\t\t");
-            System.out.println(allocation[i] != -1 ? allocation[i] + 1 : "Not Allocated");
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         // Get number of memory blocks
-        System.out.print("Enter the number of memory blocks: ");
-        int m = sc.nextInt();
-
+        System.out.println("Enter the number of blocks");
+        int blockSize = sc.nextInt();
+        
         // Get block sizes from user
-        int blockSize[] = new int[m];
+        int[] block = new int[blockSize];
         System.out.println("Enter the size of each block:");
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < blockSize; i++) {
             System.out.print("Block " + (i + 1) + ": ");
-            blockSize[i] = sc.nextInt();
+            block[i] = sc.nextInt();
         }
 
         // Get number of processes
-        System.out.print("Enter the number of processes: ");
-        int n = sc.nextInt();
-
+        System.out.println("Enter the number of processes");
+        int processCount = sc.nextInt();
+        
         // Get process sizes from user
-        int processSize[] = new int[n];
+        int[] process = new int[processCount];
         System.out.println("Enter the size of each process:");
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < processCount; i++) {
             System.out.print("Process " + (i + 1) + ": ");
-            processSize[i] = sc.nextInt();
+            process[i] = sc.nextInt();
         }
 
-        // Call the bestFit method
-        bestFit(blockSize, m, processSize, n);
+        // Array to track allocated blocks
+        int[] flag = new int[blockSize];
+        for(int i = 0; i < blockSize; i++) {
+            flag[i] = -1;
+        }
         
+        int[] output = new int[processCount];
+        for(int i = 0; i < processCount; i++) {
+            output[i] = -1;  // Initialize to -1 indicating unallocated
+        }
+
+        // Best-Fit Allocation
+        for(int m = 0; m < processCount; m++) {
+            int bestIdx = -1;
+            for(int n = 0; n < blockSize; n++) {
+                // Find the smallest suitable block for the process
+                if(process[m] <= block[n] && flag[n] == -1) {
+                    if(bestIdx == -1 || block[bestIdx] > block[n]) {
+                        bestIdx = n;
+                    }
+                }
+            }
+            if(bestIdx != -1) {
+                output[m] = bestIdx;  // Assign block index to process
+                flag[bestIdx] = 1;  // Mark block as allocated
+            }
+        }
+
+        // Display results
+        System.out.println("\nProcess No.\tProcess Size\tBlock No.");
+        for(int i = 0; i < processCount; i++) {
+            System.out.print(" " + (i + 1) + "\t\t" + process[i] + "\t\t");
+            if(output[i] != -1) {
+                System.out.println((output[i] + 1));
+            } else {
+                System.out.println("Not Allocated");
+            }
+        }
+
         sc.close();
     }
 }
